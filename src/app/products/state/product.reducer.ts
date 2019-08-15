b12f623051c51cc33b71b3a3fa634a8ffb8843ca
Emmanuel Castro <emmanuel.castro@accenture.com>
2019-08-15 09:46:24 -0500

    Initilaze ngrx in the app with products component a user component

---
+++ src/app/products/state/product.reducer.ts
@@ -0,0 +1,10 @@
+export const reducer = (state , action) => {
+    switch (action.type) {
+        case 'TOGGLE_PRODUCT_CODE':
+            return { ...state,
+                    showProductCode: action.payload
+                   };
+        default:
+            return state;
+    }
+};
