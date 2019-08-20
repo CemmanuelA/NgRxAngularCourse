39f25bd9043d64c58f73c9442825dda5b4deda3d
Emmanuel Castro <emmanuel.castro@accenture.com>
2019-08-20 15:16:11 -0500

    delete and update products

--- src/app/products/state/product.effects.ts
+++ src/app/products/state/product.effects.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { Product } from '../product';
 import { mergeMap, map, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 // services
 import { ProductService } from '../product.service';
 
 /* NgRX */
+import { Action } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as productActions from './product.action';
 
@@ -24,6 +25,42 @@ export class ProductEffects {
             map((products: Product[]) => (new productActions.LoadSuccess(products))),
             catchError(err => of(new productActions.LoadFail(err)))
         ))
+    );
+
+    @Effect()
+    updateProduct$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.UpdateProduct),
+        map((action: productActions.UpdateProduct) => action.payload),
+        mergeMap((product: Product) =>
+            this._productService.updateProduct(product).pipe(
+             map(updateProduct => (new productActions.UpdateProductSuccess(updateProduct))),
+             catchError(err => of(new productActions.UpdateProductFail(err)))
+            )
+        )
+    );
+
+    @Effect()
+    createProduct$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.CreateProduct),
+        map((action: productActions.CreateProduct) => action.payload),
+        mergeMap((product: Product) =>
+            this._productService.createProduct(product).pipe(
+                map(newProduct => (new productActions.CreateProductSuccess(newProduct))),
+                catchError(err => of(new productActions.CreateProductFail(err)))
+            )
+        )
+    );
+
+    @Effect()
+    deleteProduct$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.DeleteProduct),
+        map((action: productActions.DeleteProduct) => action.payload),
+        mergeMap((productId: number) =>
+            this._productService.deleteProduct(productId).pipe(
+                map(() => (new productActions.DeleteProductSuccess(productId))),
+                catchError(err => of(new productActions.DeleteProductFail(err)))
+            )
+        )
     );
 
 }
